a28b8a00bc5589169da692a7b89b5c4028359162
José María Sosa Resendiz <jose.skedar@gmail.com>
2021-12-17 09:57:48 -0600

    Only 1 NFT per Account

--- neavatar/src/lib.rs
+++ neavatar/src/lib.rs
@@ -34,6 +34,8 @@ near_sdk::setup_alloc!();
 pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
+    inventory: HashMap<AccountId, TokenId>,
+    token_count: u32,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -80,6 +82,8 @@ impl Contract {
                 Some(StorageKey::Approval),
             ),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+            inventory: HashMap::default(),
+            token_count: 0,
         }
     }
 
@@ -98,6 +102,9 @@ impl Contract {
         receiver_id: ValidAccountId,
         token_metadata: TokenMetadata,
     ) -> Token {
+        assert!(!self.inventory.contains_key(&receiver_id.to_string()), "Only 1 NFT by Account!");
+        self.inventory.insert(receiver_id.to_string(), self.token_count.to_string());
+        self.token_count += 1;
         self.tokens.mint(token_id, receiver_id, Some(token_metadata))
     }
 }
@@ -156,6 +163,39 @@ mod tests {
         let contract = Contract::new_default_meta(accounts(1).into());
         testing_env!(context.is_view(true).build());
         assert_eq!(contract.nft_token("1".to_string()), None);
+    }
+
+    #[test]
+    fn test_new_token() {
+        let mut context = get_context(accounts(0));
+        testing_env!(context.build());
+        let mut contract = Contract::new_default_meta(accounts(0).into());
+        testing_env!(context
+            .storage_usage(env::storage_usage())
+            .attached_deposit(MINT_STORAGE_COST)
+            .predecessor_account_id(accounts(0))
+            .build());
+        let token_id = contract.token_count.to_string();
+        let token = contract.nft_mint(token_id.clone(), accounts(2), sample_token_metadata());
+        assert_eq!(token.token_id, token_id);
+    }
+
+    #[test]
+    #[should_panic]
+    #[allow(unused_variables)]
+    fn test_one_per_account() {
+        let mut context = get_context(accounts(0));
+        testing_env!(context.build());
+        let mut contract = Contract::new_default_meta(accounts(0).into());
+        testing_env!(context
+            .storage_usage(env::storage_usage())
+            .attached_deposit(MINT_STORAGE_COST)
+            .predecessor_account_id(accounts(0))
+            .build());
+        let token_id = contract.token_count.to_string();
+        let token = contract.nft_mint(token_id.clone(), accounts(2), sample_token_metadata());
+        let next_token_id = (contract.token_count + 1).to_string();
+        let token_new = contract.nft_mint(next_token_id.clone(), accounts(2), sample_token_metadata());
     }
 
     #[test]
